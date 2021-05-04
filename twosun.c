3999a24c7a0d4cd5b35391f29d926dbcbcd15310
ryanzhu666 <zhubin911@gmail.com>
2021-05-04 13:47:10 +1000

    adding main function

---
+++ twosun.c
@@ -0,0 +1,14 @@
+// COMP9024 assignment 3
+// Almersy
+// 2021-04-25
+// Platform: Windows 10
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+
+void main(){
+
+}
